a4dda8a369c6c9b82b899043b3b3fec7fe362e20
Mart <48913272+MartKaal@users.noreply.github.com>
2023-04-29 18:02:13 +0200

    a bit cleaner code

--- script.js
+++ script.js
@@ -3,6 +3,10 @@ const button = document.querySelector('button');
 const containerWidth = 960;
 let grid = 16;
 
+let mouseDown = false;
+document.body.onmousedown = () => (mouseDown = true);
+document.body.onmouseup = () => (mouseDown = false);
+
 const renderGrid = function (grid) {
     for (let index = 0; index < grid * grid; index++) {
         let div = document.createElement('div');
@@ -16,12 +20,16 @@ const renderGrid = function (grid) {
     let items = document.querySelectorAll('.item');
     items.forEach((item) => {
         item.style.cssText = `height: ${containerWidth / grid}px; flex: 1 0 calc(100% / ${grid + 1});`;
-        item.addEventListener('mouseover', (e) => {
-            if (e.buttons == 1 || e.buttons == 3) {
-                item.style.cssText += `background-color: #000;`
-            }
-        })
+        item.addEventListener('mouseover', changeColor);
+        item.addEventListener('mousedown', changeColor);
     })
+}
+
+function changeColor(e) {
+    console.log(e.type);
+    if (e.type == 'mouseover' == !mouseDown) return;
+
+    e.target.style.backgroundColor = '#000';
 }
 
 button.addEventListener('click', (e) => {
